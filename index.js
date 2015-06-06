182423308b5da63d5770072f888fb1b54542f7a2
juno <juno@krasava.fi>
2015-06-06 08:01:14 +0300

    dev

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 #! /usr/bin/env node
 
+var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+var url = 'mongodb://localhost:27017/remotejob';
+var urlparser = require("url");
+
 console.log('filesearch');
+console.log(process.argv);
+var userArgs = process.argv.slice(2);
+var searchPattern = userArgs[0];
+console.log(searchPattern);
+
+MongoClient.connect(url,function(err, db) {
+	assert.equal(null,err);
+	
+	var collection = db.collection('employers');
+
+	
+	collection.find({"manual":true}, function(err, resultCursor) {
+	      resultCursor.each(function(err, result) {
+	    	  
+	    	  
+	    	  var toparse = result;
+	    	  
+	    	  if (toparse !== null) { 
+	    	  
+	    		  if (toparse.hasOwnProperty('extlink')) {
+
+	    			  
+	    			  var hostname = urlparser.parse(toparse.extlink).hostname;
+	    			  
+//	    			  console.log(hostname);
+	    			  
+	    			  if (hostname ==='hire.jobvite.com') {
+	    				  
+	    				  console.log(toparse.extlink);
+	    				  
+	    			  }
+	    			  
+	    			  
+	    			  
+	    		  }
+	    	  
+	    	  } else {
+	    		  db.close(); 
+	    		  
+	    	  }
+
+	    	  
+
+	      });
+	
+	});
+	
+	
+});
+	
